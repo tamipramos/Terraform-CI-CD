2190d465ddee616c88858b338c66dc521b83de92
Tami <tamipramos@gmail.com>
2026-01-20 09:52:33 +0000

    Update outputs to use private IPs and add endpoint
    Changed outputs to use private IPs for servers and added application endpoint output.

--- AWS/outputs.tf
+++ AWS/outputs.tf
@@ -1,9 +1,15 @@
 output "instance_IP_BE" {
   description = "Backend Server Public IP"
-  value       = aws_instance.BE_server.public_ip
+  value       = aws_instance.BE_server.private_ip
 }
 
 output "instance_IP_FE" {
   description = "Frontend Server Public IP"
-  value       = aws_instance.FE_server.public_ip
+  value       = aws_instance.FE_server.private_ip
 }
+
+output "application_endpoint" {
+  value       = aws_lb.app.dns_name
+  description = "Public entrypoint of the application"
+}
+

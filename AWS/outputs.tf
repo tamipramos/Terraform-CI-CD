464e3d052d6bed84f2d7e21785287ed90afc7cc4
Tami <tamipramos@gmail.com>
2026-01-20 11:04:29 +0000

    Add output for private_key in outputs.tf

--- AWS/outputs.tf
+++ AWS/outputs.tf
@@ -18,3 +18,8 @@ output "private_key_openssh" {
   value     = tls_private_key.terraform.private_key_openssh
   sensitive = true
 }
+
+output "private_key" {
+  value     = tls_private_key.terraform.private_key_pem
+  sensitive = true
+}
